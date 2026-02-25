f54d87dfaa5a9b0548428fd7e11cf0ef8531f532
jekashapar-hash <jeka.shapar@gmail.com>
2026-02-25 21:01:59 +0200

    register models

--- salocore/admin.py
+++ salocore/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Tournament)
+admin.site.register(TournamentJury)
+admin.site.register(Round)
+admin.site.register(EvaluationCriterion)
+admin.site.register(RoundRequirement)
+admin.site.register(Submission)
+admin.site.register(Evaluation)
+admin.site.register(CreterionScore)
+admin.site.register(RequirementEvaluation)
+admin.site.register(User)
+admin.site.register(UserAdminProfile)
+admin.site.register(UserJuryProfile)
+admin.site.register(UserParticipantProfile)
