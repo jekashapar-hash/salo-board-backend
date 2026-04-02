6b3494fc6c37825162f2de3e94e6991b5e9c4fc0
jekashapar-hash <jeka.shapar@gmail.com>
2026-04-02 20:37:02 +0300

    feat: add models to admin

--- salocore/admin.py
+++ salocore/admin.py
@@ -1,14 +1,40 @@
 from django.contrib import admin
-from .models import *
+from .models import (
+    User,
+    UserProfile,
+    Tournament,
+    TournamentJury,
+    TournamentAdmin as TournamentAdminModel,
+    Round,
+    EvaluationCriterion,
+    RoundRequirement,
+    RoundAttachment,
+    Team,
+    TeamMember,
+    Submission,
+    Evaluation,
+    CriterionEvaluation,
+    RequirementEvaluation,
+    Chat,
+    Message,
+    Notification,
+)
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(UserProfile)
 admin.site.register(Tournament)
 admin.site.register(TournamentJury)
+admin.site.register(TournamentAdminModel)
 admin.site.register(Round)
 admin.site.register(EvaluationCriterion)
 admin.site.register(RoundRequirement)
+admin.site.register(RoundAttachment)
+admin.site.register(Team)
+admin.site.register(TeamMember)
 admin.site.register(Submission)
 admin.site.register(Evaluation)
 admin.site.register(CriterionEvaluation)
 admin.site.register(RequirementEvaluation)
-admin.site.register(User)
+admin.site.register(Chat)
+admin.site.register(Message)
+admin.site.register(Notification)
