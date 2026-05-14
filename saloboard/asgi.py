94f42254d2e830fec01406c91b2807dd365fd1c1
jekashapar-hash <jeka.shapar@gmail.com>
2026-05-14 23:03:21 +0300

    fix: asgi.py imports

--- saloboard/asgi.py
+++ saloboard/asgi.py
@@ -8,18 +8,22 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
-
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
+# 1. Спочатку встановлюємо налаштування
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "saloboard.settings")
+
+# 2. Ініціалізуємо Django ASGI додаток (це завантажує реєстр додатків та моделі)
+django_asgi_app = get_asgi_application()
+
+# 3. ПІСЛЯ ЦЬОГО імпортуємо те, що використовує моделі
+from channels.routing import ProtocolTypeRouter, URLRouter
 import salocore.routing
 from salocore.middleware import JwtAuthMiddleware
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "saloboard.settings")
-
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": JwtAuthMiddleware(URLRouter(salocore.routing.websocket_urlpatterns)),
     }
 )
